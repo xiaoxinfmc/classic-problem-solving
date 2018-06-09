a5a781f182b74be945e504aedc3a10bad9f3057c
xiaoxinfmc <xiaoxinfmc@test.com>
2018-06-09 14:50:56 -0700

    more prob

--- chores_util.cpp
+++ chores_util.cpp
@@ -46,8 +46,8 @@ public:
    */
   static int reverse_integer(long input) {
     long long flipped_num = 0;
-    long long postive_max_abs_limit = INT_MAX;
-    long long negtive_max_abs_limit = INT_MAX + 1;
+    long long postive_max_abs_limit = (long)INT_MAX;
+    long long negtive_max_abs_limit = (long)INT_MAX + 1;
     // 1003 -> 3, 3 * 10 + 0, 30 * 10 + 0, 300 * 10 + 1
     for (int digit = 0; (input / (int)pow(10, digit)) != 0; digit++) {
       flipped_num = flipped_num * 10 + ((input / (int)pow(10, digit)) % 10);
@@ -65,6 +65,75 @@ public:
     // negative-int: 1111 1111 1111 1111 1111 1111 1111 1111 -> -1
     // negative-int: 1111 1111 1111 1111 1111 1111 1111 1110 -> -2
     return flipped_num;
+  }
+
+  /**
+   * 15.2 Palindrome Number
+   * Determine whether an integer is a palindrome. Do this without extra space.
+   * Some hints:
+   * Could negative integers be palindromes? (ie, -1)
+   * If you are thinking of converting the integer to string, note the
+   * restriction of using extra space. You could also try reversing an integer.
+   * However, if you have solved the problem Reverse Integer, you know that the
+   * reversed integer might overflow. How would you handle such case? There is
+   * a more generic way of solving this problem.
+   * -121 -> true, 0 -> true, 101 -> true
+   */
+  static bool is_palin_number(int num) {
+    int digits_cnt = 1;
+    for (int divisor = 10; num / divisor != 0; divisor = 10 * divisor) { digits_cnt++; }
+    for (int i = 0; i < digits_cnt; i++) {
+      if ((num / (int)pow(10, i)) % 10 !=
+          (num / (int)pow(10, digits_cnt - i - 1)) % 10) { return false; }
+    }
+    return true;
+  }
+
+  /**
+   * 15.3 Insert Interval
+   * Given a set of non-overlapping intervals, insert a new interval into the
+   * intervals (merge if necessary). You may assume that the intervals were
+   * initially sorted according to their start times.
+   * Ex1: Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
+   * Ex2: Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as
+   *            [1,2],[3,10],[12,16]. => [1,2], [3, 5 > 4, => (3, 9),
+   * This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+   */
+  class prob_intv {
+  public:
+    prob_intv(int s, int e) : start(s), end(e) {}
+    virtual ~prob_intv() {}
+    friend ostream & operator<< (ostream & os, const prob_intv & intv) {
+      os << "[ " << intv.start << ", " << intv.end << " ]"; return os;
+    }
+    int start, end;
+  };
+  static vector<prob_intv> merge_interval(vector<prob_intv> intv_arr,
+                                          prob_intv new_intv) {
+    vector<prob_intv> new_intv_arr; bool is_intv_inserted = false;
+    for (vector<prob_intv>::const_iterator itr = intv_arr.begin();
+         itr != intv_arr.end(); itr++) {
+      if (false == is_intv_inserted) {
+        if (new_intv.end < itr->start) { // [ns, ne ] [ x, y ]
+          new_intv_arr.push_back(new_intv);
+          new_intv_arr.push_back(* itr);
+          is_intv_inserted = true;
+        } else if (new_intv.start > itr->end){ // [ x, y ] [ ns, ne ]
+          new_intv_arr.push_back(* itr);
+        } else { // [ x, ns, y, ne ] || [ x, ns, ne, y ], [ns, x, ne, y ], [ ns, x, y, ne ]
+          new_intv_arr.push_back(prob_intv(min(itr->start, new_intv.start),
+                                           max(itr->end, new_intv.end)));
+          is_intv_inserted = true;
+        }
+      } else {
+        if (itr->start < new_intv_arr.back().end) {
+          new_intv_arr.back().end = max(itr->end, new_intv_arr.back().end);
+        } else {
+          new_intv_arr.push_back(* itr);
+        }
+      }
+    }
+    return new_intv_arr;
   }
 
   /**
@@ -202,5 +271,52 @@ int main(void) {
   cout << "==>> -1000000003: " << ChoresUtil::reverse_integer(-1000000003) << endl;
   cout << "==>> int-max: " << ChoresUtil::reverse_integer(INT_MAX) << endl;
   cout << "==>> int-min: " << ChoresUtil::reverse_integer((-1 * long(INT_MAX) + 1)) << endl;
+
+  cout << "==>> palin: 1 => 1" << ChoresUtil::is_palin_number(1) << endl;
+  cout << "==>> palin: 1 => 11" << ChoresUtil::is_palin_number(11) << endl;
+  cout << "==>> palin: 1 => 101" << ChoresUtil::is_palin_number(101) << endl;
+  cout << "==>> palin: 1 => 11011" << ChoresUtil::is_palin_number(11011) << endl;
+  cout << "==>> palin: 1 => -1" << ChoresUtil::is_palin_number(-1) << endl;
+  cout << "==>> palin: 1 => -11" << ChoresUtil::is_palin_number(-11) << endl;
+  cout << "==>> palin: 1 => -101" << ChoresUtil::is_palin_number(-101) << endl;
+  cout << "==>> palin: 0 => -1000" << ChoresUtil::is_palin_number(-1000) << endl;
+  cout << "==>> palin: 0 => -10002" << ChoresUtil::is_palin_number(-10002) << endl;
+
+  /**
+   * Ex1: Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
+   * Ex2: Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as
+   *            [1,2],[3,10],[12,16]. => [1,2], [3, 5 > 4, => (3, 9),
+   */
+  typedef ChoresUtil::prob_intv prob_intv;
+  ChoresUtil::print_all_elem<prob_intv>(
+    ChoresUtil::merge_interval(
+      vector<prob_intv>({ prob_intv(1, 3), prob_intv(6, 9) }), prob_intv(2, 5)
+    )
+  );
+  ChoresUtil::print_all_elem<prob_intv>(
+    ChoresUtil::merge_interval(
+      vector<prob_intv>({ prob_intv(1, 2), prob_intv(3, 5), prob_intv(6, 7), prob_intv(8, 10), prob_intv(12, 16) }), prob_intv(4, 9)
+    )
+  );
+  ChoresUtil::print_all_elem<prob_intv>(
+    ChoresUtil::merge_interval(
+      vector<prob_intv>({ prob_intv(1, 2), prob_intv(3, 5), prob_intv(8, 10), prob_intv(12, 16) }), prob_intv(6, 7)
+    )
+  );
+  ChoresUtil::print_all_elem<prob_intv>(
+    ChoresUtil::merge_interval(
+      vector<prob_intv>({ prob_intv(1, 2), prob_intv(3, 5), prob_intv(8, 10), prob_intv(12, 16) }), prob_intv(0, 17)
+    )
+  );
+  ChoresUtil::print_all_elem<prob_intv>(
+    ChoresUtil::merge_interval(
+      vector<prob_intv>({ prob_intv(1, 2), prob_intv(3, 5), prob_intv(8, 10), prob_intv(12, 16) }), prob_intv(0, 11)
+    )
+  );
+  ChoresUtil::print_all_elem<prob_intv>(
+    ChoresUtil::merge_interval(
+      vector<prob_intv>({ prob_intv(1, 2), prob_intv(3, 5), prob_intv(8, 10), prob_intv(12, 16) }), prob_intv(9, 17)
+    )
+  );
   return 0;
 }
