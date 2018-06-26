4eec2eb741118d989dac907ec2bbfeaead87bcb4
xiaoxinfmc <xiaoxinfmc@test.com>
2018-06-25 22:48:44 -0700

    list

--- list_util.cpp
+++ list_util.cpp
@@ -4,11 +4,10 @@
  * sorted list. The given node can be any single node in the list.
  */
 #include <iostream>
-#include <cassert>
+#include <vector>
 
 using namespace std;
 
-class ListUtil {
 /**
  * 1 -> 3 -> 3 -> 7
  * ^--------------|
@@ -16,78 +15,72 @@ class ListUtil {
  * 1 -> 3 -> 3 -> (3) -> 7
  * ^---------------------|
  * needs check back to origin
- * case I: given 3, => loop next until val > 3 or start <.
- * case II: given 1, => loop next val > 3 or start <.
- * case IV: given 7, => loop next val > 3 && val < 7 or start <.
+ * case I:  given 3, => loop next until val > 3 or start decr.
+ * case II: given 1, => loop next val > 3 or start decr.
+ * case IV: given 7, => loop next val > 3 && val < 7 or start decr.
  */
-public:
-  class CyclicSortedListNode {
+namespace list_util {
+  class circular_sorted_list_node {
   public:
-    CyclicSortedListNode(int val) { value = val; next_ptr = this; }
-    virtual ~CyclicSortedListNode() { delete next_ptr; next_ptr = NULL; }
-    CyclicSortedListNode * next_ptr;
+    circular_sorted_list_node(int val) { value = val; next_ptr = this; }
+    virtual ~circular_sorted_list_node() {}
+    circular_sorted_list_node * next_ptr;
     int value;
   };
+  static circular_sorted_list_node *
+    insert_circular_sorted_list(circular_sorted_list_node * list_head_ptr,
+                                int value_to_insert) {
+    circular_sorted_list_node * inst_node_ptr = new circular_sorted_list_node(value_to_insert);
+    if (NULL == list_head_ptr) { return inst_node_ptr; }
 
-  static CyclicSortedListNode * insert_cyclic_sorted_list(
-    CyclicSortedListNode * list_head_ptr, int value_to_insert)
-  {
-    CyclicSortedListNode * insert_node_ptr =new CyclicSortedListNode(value_to_insert);
-    CyclicSortedListNode * curr_node_ptr = list_head_ptr;
+    circular_sorted_list_node * curr_node_ptr = list_head_ptr,
+                              * temp_node_ptr = NULL;
 
-    if (NULL == list_head_ptr) { return insert_node_ptr; }
-
-    // (3),  1, 3, 3, 5, 7(given), 9 loop until 1 (curr_ptr -> 9, 1 <= 3)
-    // or back to 7, say 6 7 7 (7) (curr_ptr -> last 7)
-    // or back to 7, say 6 7 (7) 7 (curr_ptr -> last 7)
-    do {
-      if ((value_to_insert >= curr_node_ptr->value) &&
-          (value_to_insert < curr_node_ptr->next_ptr->value) &&
-          (curr_node_ptr->value < curr_node_ptr->next_ptr->value)) {
-        break;
-      } else if ((curr_node_ptr->value > curr_node_ptr->next_ptr->value) &&
-                 (value_to_insert >= curr_node_ptr->value)) {
-        break;
-      } else {
+    if (curr_node_ptr == curr_node_ptr->next_ptr) {
+      list_head_ptr->next_ptr = inst_node_ptr;
+      inst_node_ptr->next_ptr = list_head_ptr;
+    } else {
+      do {
+        if ((curr_node_ptr->value <= inst_node_ptr->value &&           // mid
+             inst_node_ptr->value <  curr_node_ptr->next_ptr->value) ||
+            (curr_node_ptr->value >  curr_node_ptr->next_ptr->value && // min
+             curr_node_ptr->next_ptr->value >= inst_node_ptr->value) ||
+            (curr_node_ptr->value >  curr_node_ptr->next_ptr->value && // max
+             curr_node_ptr->value <= inst_node_ptr->value) ||
+            (curr_node_ptr->next_ptr == list_head_ptr)) {              // eql
+          temp_node_ptr = curr_node_ptr->next_ptr;
+          curr_node_ptr->next_ptr = inst_node_ptr;
+          inst_node_ptr->next_ptr = temp_node_ptr;
+          break;
+        }
         curr_node_ptr = curr_node_ptr->next_ptr;
-      }
-    } while (list_head_ptr != curr_node_ptr);
-
-    insert_node_ptr->next_ptr = curr_node_ptr->next_ptr;
-    curr_node_ptr->next_ptr = insert_node_ptr;
-
-    return insert_node_ptr;
+      } while (curr_node_ptr != list_head_ptr);
+    }
+    return inst_node_ptr;
   }
-
-  static void print_list(CyclicSortedListNode * root_ptr) {
-    if (NULL == root_ptr) { return; }
-    CyclicSortedListNode * curr_node_ptr = root_ptr; ;
+  static void print_circular_sorted_list(circular_sorted_list_node * list_head_ptr) {
+    if (NULL == list_head_ptr) { return; }
+    circular_sorted_list_node * curr_node_ptr = list_head_ptr;
     do {
       cout << curr_node_ptr->value << " ";
       curr_node_ptr = curr_node_ptr->next_ptr;
-    } while (curr_node_ptr != root_ptr); cout << endl;
+    } while (curr_node_ptr != list_head_ptr); cout << endl;
   }
 };
 
 int main(void) {
-  ListUtil::CyclicSortedListNode * list_one =
-    ListUtil::insert_cyclic_sorted_list(NULL, 1);
-  ListUtil::print_list(list_one);
-  list_one = ListUtil::insert_cyclic_sorted_list(list_one, 1);
-  ListUtil::print_list(list_one);
-  list_one = ListUtil::insert_cyclic_sorted_list(list_one, 1);
-  ListUtil::print_list(list_one);
-  list_one = ListUtil::insert_cyclic_sorted_list(list_one, 3);
-  ListUtil::print_list(list_one);
-  list_one = ListUtil::insert_cyclic_sorted_list(list_one, -1);
-  ListUtil::print_list(list_one);
-  list_one = ListUtil::insert_cyclic_sorted_list(list_one, -1);
-  ListUtil::print_list(list_one);
-  list_one = ListUtil::insert_cyclic_sorted_list(list_one, 5);
-  ListUtil::print_list(list_one);
-  list_one = ListUtil::insert_cyclic_sorted_list(list_one, 5);
-  ListUtil::print_list(list_one);
-  list_one = ListUtil::insert_cyclic_sorted_list(list_one, 2);
-  ListUtil::print_list(list_one);
+  using list_util::circular_sorted_list_node;
+  using list_util::insert_circular_sorted_list;
+  using list_util::print_circular_sorted_list;
+
+  cout << "1. insert_circular_sorted_list:" << endl;
+  cout << "-1 -1 0 1 1 2 3 5 5" << endl;
+  vector<int> elem_value_arr({ -1, -1, 1, 1, 2, 3, 5, 5 });
+  circular_sorted_list_node * list_head_ptr = new circular_sorted_list_node(0);
+  for (auto & elem_value : elem_value_arr) {
+    list_head_ptr = insert_circular_sorted_list(list_head_ptr, elem_value);
+    print_circular_sorted_list(list_head_ptr);
+  }
+  print_circular_sorted_list(list_head_ptr);
   return 0;
 }
