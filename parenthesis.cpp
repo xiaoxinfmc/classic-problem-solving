1ca64daa5970c31d3e374820f021436e06330d48
xiaoxinfmc <xiaoxinfmc@test.com>
2018-08-19 14:34:06 -0700

    impl

--- parenthesis.cpp
+++ parenthesis.cpp
@@ -9,40 +9,36 @@ using namespace std;
  * (()()), (())(), ()()(), ((())), good 
  * ()()(), ()(()),  (()()), ((()))
  * ())((), ()))((, )))(((, bad
- * can be generalized to choose whether or not to enclose the pair each time
- * n pairs of parentheses, 2n tokens, 2n!, needs pruning.
- * number of '(' and ')' must match in the end.
- * always start with '(', when started or all previous () enclosed with each other
- * for any valid pairs(enclosed), '(' and ')' match the count
- * anytime during the logic, left parts must be >= right parts
+ *
+ * Observation:
+ * - all valid combination has to start with '(', end with ')', # has to match.
+ * - at any time, # of '(' is no less than # of ')'
+ * - we form a good comb if reach end & conform all valid conditions @ each step
  */
 class ParenthesisUtil {
 public:
+
   static vector<string> generate_parenthesis(int pair_cnt) {
-    vector<string> pairs;
-    generate_parenthesis_comb(0, 0, pair_cnt, "", pairs);
-    return pairs;
+    vector<string> paren_comb_ret;
+    gen_valid_parenthesis_recur(0, 0, pair_cnt, "", paren_comb_ret);
+    return paren_comb_ret;
   }
-  static void generate_parenthesis_comb(int left_cnt, int right_cnt,
-                                        int max_pair_cnt, string curr_prefix,
-                                        vector<string> & pairs_arr_ret) {
-    /* if we just use up all parts, then just return */
-    if ((left_cnt > max_pair_cnt) || (right_cnt > max_pair_cnt)) { return; }
-    if (curr_prefix.size() == max_pair_cnt * 2) {
-      pairs_arr_ret.push_back(curr_prefix); return;
+
+  static void gen_valid_parenthesis_recur(int left_char_cnt, int right_char_cnt,
+                                          int pair_cnt, string curr_parenthesis,
+                                          vector<string> & paren_comb_ret) {
+    if (left_char_cnt > pair_cnt || right_char_cnt > pair_cnt) { return; }
+    if (left_char_cnt == pair_cnt && right_char_cnt == pair_cnt) {
+      paren_comb_ret.push_back(curr_parenthesis); return;
     }
-    if (left_cnt == right_cnt) {
-      /* either we just start or previous parts already enclosed */
-      generate_parenthesis_comb(
-        left_cnt + 1, right_cnt, max_pair_cnt, curr_prefix + "(", pairs_arr_ret
-      );
-    } else if (left_cnt > right_cnt) {
-      generate_parenthesis_comb(
-        left_cnt + 1, right_cnt, max_pair_cnt, curr_prefix + "(", pairs_arr_ret
-      );
-      generate_parenthesis_comb(
-        left_cnt, right_cnt + 1, max_pair_cnt, curr_prefix + ")", pairs_arr_ret
-      );
+    if (left_char_cnt == right_char_cnt) {
+      gen_valid_parenthesis_recur(left_char_cnt + 1, right_char_cnt, pair_cnt,
+                                  curr_parenthesis + "(", paren_comb_ret);
+    } else if (left_char_cnt > right_char_cnt) {
+       gen_valid_parenthesis_recur(left_char_cnt + 1, right_char_cnt, pair_cnt,
+                                   curr_parenthesis + "(", paren_comb_ret);
+       gen_valid_parenthesis_recur(left_char_cnt, right_char_cnt + 1, pair_cnt,
+                                   curr_parenthesis + ")", paren_comb_ret);
     }
   }
 };
