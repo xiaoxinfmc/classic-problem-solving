50e2f4698ef8035f96791716bbadb110cdba849a
xiaoxinfmc <xiaoxinfmc@test.com>
2018-09-03 19:06:31 -0700

    more-board

--- array_util.cpp
+++ array_util.cpp
@@ -796,6 +796,84 @@ namespace array_util {
     } while (fast_ptr != slow_ptr);
     return slow_ptr;
   }
+
+  /**
+   * 289. Game of Life
+   * According to the Wikipedia article: "The Game of Life, also known simply
+   * as Life, is a cellular automaton devised by the British mathematician John
+   * Horton Conway in 1970."
+   * Given a board with m by n cells, each cell has an initial state live (1)
+   * or dead (0). Each cell interacts with its eight neighbors (horizontal,
+   * vertical, diagonal) using the following four rules (taken from the above
+   * Wikipedia article):
+   *
+   * - Any live cell with < 2 live neighbors dies, as if by under-population.
+   * - Any live cell with 2|3 live neighbors lives on to next generation.
+   * - Any live cell with > 3 live neighbors dies, as if by over-population.
+   * - Any dead cell with 3 live neighbors is live, as if by reproduction.
+   *
+   * Write a function to compute the next state (after one update) of the
+   * board given its current state. The next state is created by applying
+   * the above rules simultaneously to every cell in the current state, where
+   * births and deaths occur simultaneously.
+   *
+   * Example:
+   * - Input:
+   * [ [0,1,0],
+   *   [0,0,1],
+   *   [1,1,1],
+   *   [0,0,0] ]
+   * - Output:
+   * [ [0,0,0],
+   *   [1,0,1],
+   *   [0,1,1],
+   *   [0,1,0] ]
+   *
+   * Follow up:
+   * Could you solve it in-place? Remember that the board needs to be updated at
+   * the same time: You cannot update some cells first and then use their
+   * updated values to update other cells.
+   * In this question, we represent the board using a 2D array. In principle,
+   * the board is infinite, which would cause problems when the active area
+   * encroaches the border of the array. How would you address these problems?
+   * Observation:
+   * - to update the board in place requires us not to erode any info from input.
+   * - but the status of any cell is just 2, either 0 or 1, if no change update.
+   * - 0 -> 1 then we can say 2, if 1 -> 0, then we can use 3.
+   */
+  static void check_and_update_board(vector<vector<int>> & board) {
+    enum { DEAD = 0, LIVE = 1, DEAD_TO_LIVE = 2, LIVE_TO_DEAD = 3 };
+
+    for (int i = 0; i < board.size(); i++) {
+      for (int j = 0; j < board[i].size(); j++) {
+
+        int live_neighbor_cnt = 0;
+        for (int x = i - 1; x <= i + 1; x++) {
+          for (int y = j - 1; y <= j + 1; y++) {
+            if (x == i && y == j) { continue; }
+            if (x >= 0 && x < board.size() && y >= 0 && y < board[x].size()) {
+              if (LIVE == board[x][y] || LIVE_TO_DEAD == board[x][y]) { live_neighbor_cnt++; }
+            }
+          }
+        }
+
+        switch(live_neighbor_cnt) {
+          case 0:
+          case 1: { if (LIVE == board[i][j]) { board[i][j] = LIVE_TO_DEAD; } break; }
+          case 2:
+          case 3: { if (3 == live_neighbor_cnt && DEAD == board[i][j]) { board[i][j] = DEAD_TO_LIVE; } break; }
+          default: { if (LIVE == board[i][j]) { board[i][j] = LIVE_TO_DEAD; } break; }
+        }
+      }
+    }
+
+    for (int i = 0; i < board.size(); i++) {
+      for (int j = 0; j < board[i].size(); j++) {
+        if (DEAD_TO_LIVE == board[i][j]) { board[i][j] = LIVE; }
+        else if (LIVE_TO_DEAD == board[i][j]) { board[i][j] = DEAD; }
+      }
+    }
+  }
 };
 
 int main(void) {
@@ -814,6 +892,7 @@ int main(void) {
   using array_util::is_sudoku_valid;
   using array_util::solve_sudoku;
   using array_util::find_duplicate;
+  using array_util::check_and_update_board;
 
   cout << "1. get_next_permutation_asc" << endl;
   cout << "[ 6 8 1 3 7 4 0 1 2 3 ] <=> " << endl;
@@ -926,5 +1005,15 @@ int main(void) {
   cout << "13. find_duplicate" << endl;
   cout << "2 <=> " << find_duplicate(vector<int>({ 1, 3, 4, 2, 2 })) << endl;
   cout << "3 <=> " << find_duplicate(vector<int>({ 3, 1, 3, 5, 2 })) << endl;
+
+  cout << "14. check_and_update_board" << endl;
+  vector<vector<int>> board({{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+                             { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+                             { 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+                             { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }});
+  print_all_elem_vec<int>(board);
+  check_and_update_board(board);
+  print_all_elem_vec<int>(board);
   return 0;
 }
