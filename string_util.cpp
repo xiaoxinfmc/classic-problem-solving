5a376332abe955ba4f165b3773f4c0d9cce521ea
xiaoxinfmc <xiaoxinfmc@test.com>
2018-09-19 00:49:35 -0700

    impl

--- string_util.cpp
+++ string_util.cpp
@@ -3,116 +3,184 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
+#include <unordered_set>
 
-using namespace std;
+namespace string_util {
+  using std::cout;
+  using std::endl;
+  using std::string;
+  using std::vector;
+  using std::unordered_set;
 
-class StringUtil{
-public:
-  /*
-   * s: $  a  $  b  $  a  $  b  $  a  $  b  $  a  $
-   * i:    1  2
-   * r: 0  1  ?
-   *  2-1-1 2-1+1
-   *    0     2
-   *    0  1  2  3
-   *    0  1  0  ?(i=3)
-   *             1
-   *      3-1-0 <=> 3-1+0 ~ 2
-   * s: $  a  $  b  $  a  $  b  $  a  $  b  $  a  $
-   * i: 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14
-   * l: 0  1  0  3  0  5  0  7  0  5  0  3  0  1  0
-   *    |--------------|--?-----------|
-   *         m       m*
-   *    |<<<<<<<<|>>>>>>>>|
-   *                    m        m*
-	 *             ***|<<<<<<<<|>>>>>>>>|***
-   *    |????????????        ^         ???????????|
-   *
-   *    |<<<<<<<<<<<<<<<<<<<<|>>>>>>>>>>>>>>>>>>>>|
-   *            |-----| r_without_cp
-   *   lbi     c     rbi
-   *    |<<<<<<|>>>>>>|      ^    **|<<<<<<|>>>>>>|**
-   *                  |<<|>>|^|<<|>>|            ==>> case I
+  template <class type>
+  static void print_all_elem(const vector<type>& input) {
+    cout << "[ ";
+    for (auto & arr : input) { cout << arr << " "; }
+    cout << "]" << endl;
+  }
+
+  template <class type>
+  static void print_all_elem_vec(const vector<vector<type>> & input) {
+    cout << "[" << endl;
+    for (auto & arr : input){ cout << "  "; print_all_elem<type>(arr); }
+    cout << "]" << endl;
+  }
+
+  /**
+   * 212. Word Search II
+   * Given a 2D board and words from the dictionary, find all words in the board.
+   * Each word must be constructed from letters of sequentially adjacent cell,
+   * where "adjacent" cells are those horizontally or vertically neighboring. The
+   * same letter cell may not be used more than once in a word.
+   * Example:
+   * Input:
+   * words = ["oath","pea","eat","rain"] and board =
+   * [
+   *   ['o','a','a','n'],
+   *   ['e','t','a','e'],
+   *   ['i','h','k','r'],
+   *   ['i','f','l','v']
+   * ]
+   * Output: ["eat","oath"]
    */
-  static string get_longest_palindrome(string input_str) {
-    string input_str_copy = "$", result = "";
-    if (true == input_str.empty()) { return result; }
-    for (int i = 0; i < input_str.size(); i++) {
-      input_str_copy += input_str[i]; input_str_copy += "$";
+  class prefix_trie_node {
+  public:
+    prefix_trie_node(char val) : char_val(val) {
+      parent_ptr = NULL;
+      for (int i = 0; i < sizeof(adjacent_ptrs)/sizeof(adjacent_ptrs[0]); i++) {
+        adjacent_ptrs[i] = NULL;
+      }
+      is_visited_flag = false;
     }
 
-    // center point will not be included in radius for a boundary.
-    int left_boundary_id = 0, right_boundary_id = 0, boundary_radius = 0;
-    int curr_left_boundary_id = 0, curr_right_boundary_id = 0;
-    int * boundary_radius_buffer_ptr = new int[input_str_copy.size()];
-    assert(NULL != boundary_radius_buffer_ptr);
+    virtual ~prefix_trie_node() {}
 
-    for (int i = 0; i < input_str_copy.size(); i++) { boundary_radius_buffer_ptr[i] = 0; }
-// cout << "radiu: "; for (int i = 0; i < input_str_copy.size(); i++) { cout << boundary_radius_buffer_ptr[i] << "\t"; } cout << endl;
-
-    boundary_radius_buffer_ptr[0] = 0; boundary_radius_buffer_ptr[1] = 1;
-    for (int i = 2; i < input_str_copy.size(); i++) {
-      if ((input_str_copy.size() - 1) == i) {
-        boundary_radius_buffer_ptr[i] = 0; continue;
-      }
-      left_boundary_id = i - 1 - boundary_radius_buffer_ptr[i - 1];
-      right_boundary_id = i - 1 + boundary_radius_buffer_ptr[i - 1];
-      if (i < right_boundary_id) {
-        boundary_radius_buffer_ptr[i] = boundary_radius_buffer_ptr[i - 2];
-        curr_right_boundary_id = i + boundary_radius_buffer_ptr[i - 2];
-        curr_right_boundary_id = (
-          curr_right_boundary_id <= right_boundary_id ? curr_right_boundary_id : right_boundary_id
-        );
-        curr_left_boundary_id = 2 * i - curr_right_boundary_id;
-        curr_left_boundary_id--; curr_right_boundary_id++;
-      } else {
-        curr_left_boundary_id = i - 1;
-        curr_right_boundary_id = i + 1;
-      }
-// cout << "i: " << i << " l: " << curr_left_boundary_id << " r: " << curr_right_boundary_id << endl;
-      while ((curr_left_boundary_id >= 0) && (curr_right_boundary_id < input_str_copy.size()) &&
-             (input_str_copy[curr_left_boundary_id] == input_str_copy[curr_right_boundary_id])) {
-        boundary_radius_buffer_ptr[i]++;
-        curr_left_boundary_id--; curr_right_boundary_id++;
-      }
-		}
-
-    int max_boundary_len = 0, max_pivot_idx = 0;
-		for (int i = 0; i < input_str_copy.size(); i += 1) {
-      if (boundary_radius_buffer_ptr[i] <= max_boundary_len) { continue; }
-      max_boundary_len = boundary_radius_buffer_ptr[i]; max_pivot_idx = i;
-    }
-    for (int i = (max_pivot_idx - max_boundary_len + 1);
-             i < (max_pivot_idx + max_boundary_len); i += 2) {
-      result += input_str_copy[i];
+    void insert_adjacent_char(prefix_trie_node * char_ptr) {
+      adjacent_ptrs[int(char_ptr->get_char_val()) - int('a')] = char_ptr;
+      char_ptr->parent_ptr = this;
     }
 
-    delete [] boundary_radius_buffer_ptr;
-// cout << "input: " << input_str << endl;
-// cout << "copys: "; for (int i = 0; i < input_str_copy.size(); i++) { cout << input_str_copy[i] << "\t"; } cout << endl;
-// cout << "index: "; for (int i = 0; i < input_str_copy.size(); i++) { cout << i << "\t"; } cout << endl;
-// cout << "radiu: "; for (int i = 0; i < input_str_copy.size(); i++) { cout << boundary_radius_buffer_ptr[i] << "\t"; } cout << endl;
-// cout << "expss: "; int x[] = { 0, 1, 0, 3, 0, 1, 0, 7, 0, 1, 0, 9, 0, 1, 0, 5, 0, 1, 0, 1, 0, 1, 2, 1, 0, 1, 0, 1, 0 }; for (int i = 0; i < sizeof(x)/sizeof(int); i++) { cout << x[i] << "\t"; } cout << endl;
-// cout << "max_p: " << max_pivot_idx << endl;
-// cout << "max_b: " << max_boundary_len << endl;
-// cout << "maxpl: "; cout << result << endl;
-    return result;
+    bool is_adjacent_char_exists(char curr_char) {
+      return (NULL != adjacent_ptrs[int(curr_char) - int('a')]);
+    }
+
+    prefix_trie_node * get_parent_ptr() { return parent_ptr; }
+
+    prefix_trie_node * get_adjacent_char_ptr(char curr_char) {
+      return adjacent_ptrs[int(curr_char) - int('a')];
+    }
+
+    char get_char_val() { return char_val; }
+
+    bool is_ending_char() {
+      for (int i = 0; i < sizeof(adjacent_ptrs)/sizeof(adjacent_ptrs[0]); i++) {
+        if (adjacent_ptrs[i] != NULL) { return false; }
+      }
+      return true;
+    }
+
+    void mark_as_visited() { is_visited_flag = true; }
+
+    bool is_visited() { return is_visited_flag; }
+
+  private:
+    char char_val;
+    bool is_visited_flag;
+    prefix_trie_node * parent_ptr;
+    prefix_trie_node * adjacent_ptrs[int('z') - int('a') + 1];
+  };
+
+  static prefix_trie_node * build_prefix_trie(const vector<string> & words) {
+    prefix_trie_node * trie_root = NULL;
+    if (true == words.empty()) { return trie_root; }
+    trie_root = new prefix_trie_node('$');
+    assert(NULL != trie_root);
+    for (auto & word : words) {
+      prefix_trie_node * curr_trie_ptr = trie_root;
+      for (auto & chr : word) {
+        if (false == curr_trie_ptr->is_adjacent_char_exists(chr)) {
+          curr_trie_ptr->insert_adjacent_char(new prefix_trie_node(chr));
+        }
+        curr_trie_ptr = curr_trie_ptr->get_adjacent_char_ptr(chr);
+      }
+    }
+    return trie_root;
+  }
+
+  static int gen_position_key(int curr_row, int curr_col,
+                              const vector<vector<char>> & board) {
+    return curr_row * board.front().size() + curr_col;
+  }
+
+  static void search_word_in_batch(const vector<vector<char>> & board,
+                                   prefix_trie_node * trie_root_ptr,
+                                   vector<string> & existing_words,
+                                   int curr_row, int curr_col,
+                                   unordered_set<int> & position_taken) {
+    if (NULL == trie_root_ptr) { return; }
+    if (curr_row >= board.size() || curr_col >= board.front().size()) { return; }
+
+    int curr_position_key = gen_position_key(curr_row, curr_col, board);
+
+    if (position_taken.end() != position_taken.find(curr_position_key)) { return; }
+
+    if (trie_root_ptr->get_char_val() != board[curr_row][curr_col]) { return; }
+
+    position_taken.insert(curr_position_key);
+
+    if (true == trie_root_ptr->is_ending_char() && false == trie_root_ptr->is_visited()) {
+      string curr_word;
+      trie_root_ptr->mark_as_visited();
+      for (; NULL != trie_root_ptr->get_parent_ptr();
+             trie_root_ptr = trie_root_ptr->get_parent_ptr()) {
+        curr_word.push_back(trie_root_ptr->get_char_val());
+      }
+      existing_words.push_back(string(curr_word.rbegin(), curr_word.rend()));
+    }
+
+    prefix_trie_node * adjacent_ptr = NULL;
+    for (int chr = int('a'); chr <= int('z'); chr++) {
+      adjacent_ptr = trie_root_ptr->get_adjacent_char_ptr(char(chr));
+      if (NULL == adjacent_ptr) { continue; }
+      search_word_in_batch(board, adjacent_ptr, existing_words, curr_row + 1, curr_col, position_taken);
+      search_word_in_batch(board, adjacent_ptr, existing_words, curr_row - 1, curr_col, position_taken);
+      search_word_in_batch(board, adjacent_ptr, existing_words, curr_row, curr_col + 1, position_taken);
+      search_word_in_batch(board, adjacent_ptr, existing_words, curr_row, curr_col - 1, position_taken);
+    }
+
+    position_taken.erase(curr_position_key);
+  }
+
+  static vector<string> find_word_in_batch(const vector<vector<char>> & board,
+                                           const vector<string> & words) {
+    vector<string> existing_words;
+    prefix_trie_node * trie_root_ptr = build_prefix_trie(words);
+    unordered_set<int> position_taken;
+
+    for (int i = 0; i < board.size(); i++) {
+      for (int j = 0; j < board[i].size(); j++) {
+        search_word_in_batch(board, trie_root_ptr->get_adjacent_char_ptr(board[i][j]),
+                             existing_words, i, j, position_taken);
+        if (existing_words.size() == words.size()) { return existing_words; }
+      }
+    }
+    return existing_words;
+  }
+
+  static void test_find_word_in_batch() {
+    cout << "1. test_find_word_in_batch" << endl;
+    cout << "[ eat oath ] <=>" << endl;
+    print_all_elem(find_word_in_batch(vector<vector<char>>({ { 'o','a','a','n' }, { 'e','t','a','e' },
+                                                             { 'i','h','k','r' }, { 'i','f','l','v' } }),
+                                      vector<string>({ "oath", "pea", "eat", "rain" })));
+    cout << "[ a ] <=>" << endl;
+    print_all_elem(find_word_in_batch(vector<vector<char>>({ { 'o','a','a','n' }, { 'e','t','a','e' },
+                                                             { 'i','h','k','r' }, { 'i','f','l','v' } }),
+                                      vector<string>({ "a" })));
   }
 };
 
 int main(void) {
-// babcbabcbaccba
-  string test_input[] = {
-    "babcbabcbaccba", "abaaba", "abababa", "abcbabcbabcba", "forgeeksskeegfor",
-    "caba", "abacdfgdcaba", "abacdfgdcabba", "abacdedcaba", "a", "aa", "",
-  };
-  string test_output[] = {
-    "abcbabcba", "abaaba", "abababa", "abcbabcbabcba", "geeksskeeg",
-    "aba", "aba", "abba", "abacdedcaba", "a", "aa", "",
-  };
-  for (int i = 0; i < sizeof(test_output) / sizeof(string); i++) {
-// cout << "expect: " << test_output[i] << endl;
-    assert(StringUtil::get_longest_palindrome(test_input[i]) == test_output[i]);
-  }
+  string_util::test_find_word_in_batch();
   return 0;
 }
