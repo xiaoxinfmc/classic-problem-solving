9c8deaaacf41e211c974293bf830935911bc2c65
xiaoxinfmc <xiaoxinfmc@test.com>
2018-10-12 12:32:50 -0700

    impl

--- array_util.cpp
+++ array_util.cpp
@@ -1677,6 +1677,69 @@ namespace array_util {
       assert(cnt == test_output[i]);
     }
   }
+
+  /**
+   * 684. Redundant Connection
+   * - In this problem, a tree is an undirected graph that is connected and
+   *   has no cycles.
+   * - The given input is a graph that started as a tree with N nodes (with
+   *   distinct values 1, 2, ..., N), with one additional edge added. The
+   *   added edge has two different vertices chosen from 1 to N, and was not
+   *   an edge that already existed.
+   * - The resulting graph is given as a 2D-array of edges. Each element of
+   *   edges is a pair [u, v] with u < v, that represents an undirected edge
+   *   connecting nodes u and v.
+   * - Return an edge that can be removed so that the resulting graph is a
+   *   tree of N nodes. If there are multiple answers, return the answer that
+   *   occurs last in the given 2D-array. The answer edge [u, v] should be in
+   *   the same format, with u < v.
+   * Example 1:
+   * - Input: {{1,2}, {1,3}, {2,3}}
+   *   Output: {2,3}
+   *   Explanation: The given undirected graph will be like this:
+   *     1
+   *    / \
+   *   2 - 3
+   * Example 2:
+   * - Input: {{1,2}, {2,3}, {3,4}, {1,4}, {1,5}}
+   *   Output: {1,4}
+   *   Explanation: The given undirected graph will be like this:
+   *   5 - 1 - 2
+   *       |   |
+   *       4 - 3
+   * Note:
+   * - The size of the input 2D-array will be between 3 and 1000.
+   * - Every integer represented in the 2D-array will be between 1 and N,
+   *   where N is the size of the input array.
+   * Intuition:
+   * - a necessary edge means it will connect parts when it is been added.
+   * - otherwise, it is redundant.
+   */
+  static vector<int> get_redundant_edges(const vector<vector<int>> & udag) {
+    vector<int> redundant_edge;
+    int prev_comp_cnt = 0;
+    disjoint_set edge_set(udag.size());
+    for (auto & pair : udag) {
+      edge_set.union_set(pair.front() - 1, pair.back() - 1);
+      if (prev_comp_cnt == edge_set.get_components_cnt()) {
+        redundant_edge = pair; break;
+      } else {
+        prev_comp_cnt = edge_set.get_components_cnt();
+      }
+    }
+    return redundant_edge;
+  }
+
+  static void test_get_redundant_edges() {
+    vector<vector<int>> test_output = { {2,3}, {1,4} };
+    vector<vector<vector<int>>> test_input = { {{1,2}, {1,3}, {2,3}}, {{1,2}, {2,3}, {3,4}, {1,4}, {1,5}} };
+    cout << "27. test_get_redundant_edges" << endl;
+    for (int i = 0; i < test_input.size(); i++) {
+      print_all_elem<int>(test_output[i]);
+      cout << "<=>" << endl;
+      print_all_elem<int>(get_redundant_edges(test_input[i]));
+    }
+  }
 };
 
 int main(void) {
@@ -1710,6 +1773,7 @@ int main(void) {
   using array_util::test_lean_calc_max_profit_iii;
   using array_util::test_convert_fraction_to_decimal;
   using array_util::test_calc_friend_circle_cnt;
+  using array_util::test_get_redundant_edges;
 
   cout << "1. get_next_permutation_asc" << endl;
   cout << "[ 6 8 1 3 7 4 0 1 2 3 ] <=> " << endl;
@@ -1847,6 +1911,7 @@ int main(void) {
   test_lean_calc_max_profit_iii();
   test_convert_fraction_to_decimal();
   test_calc_friend_circle_cnt();
+  test_get_redundant_edges();
 
   return 0;
 }
